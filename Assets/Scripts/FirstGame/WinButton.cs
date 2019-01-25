480aa6cbd1db1cf8d3631c2cb3e6db5fe9957da1
Daniel Koch <daniel.koch@helsinki.fi>
2019-01-25 12:47:54 +0200

    placeholder game has a win button and a slider-timer

---
+++ Assets/Scripts/FirstGame/WinButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class WinButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadLevelMenu()
+    {
+        SceneManager.LoadScene("Assets/Scenes/MainMenu.unity");
+    }
+}
