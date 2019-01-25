5489e329736e36dc93062ad0f0a0cb3200ed896c
anonOstrich <jesper.kuutti@helsinki.fi>
2019-01-25 13:01:51 +0200

    Created a script for using the form user data with WebServiceScript

---
+++ Assets/Scripts/FormController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class FormController : MonoBehaviour
+{
+    [SerializeField] private WebServiceScript webScript;  
+
+    // Placeholder while the user data is not saved. 
+    // Currently we have to pick the values from the text fields
+
+    public void SendFormData(){
+        string userName = GameObject.Find("Username Input").transform.Find("Text").GetComponent<Text>().text;
+        string token = GameObject.Find("Token Input").transform.Find("Text").GetComponent<Text>().text;
+        string installationId = Random.Range(0, 100000).ToString();
+        webScript.RegisterUser(userName, token, installationId); 
+        }
+
+
+}
