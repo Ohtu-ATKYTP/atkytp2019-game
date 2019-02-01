9b31d5848a17a0bd7b0d05f274c8a429101a2ba2
anonOstrich <jesper.kuutti@helsinki.fi>
2019-02-01 13:19:58 +0200

    Updating highscore should work on Unity end

--- Assets/Scripts/FormController.cs
+++ Assets/Scripts/FormController.cs
@@ -7,14 +7,10 @@ public class FormController : MonoBehaviour
 {
     [SerializeField] private WebServiceScript webScript;  
 
-    // Placeholder while the user data is not saved. 
-    // Currently we have to pick the values from the text fields
-
     public void SendFormData(){
         string userName = GameObject.Find("Username Input").transform.Find("Text").GetComponent<Text>().text;
         string token = GameObject.Find("Token Input").transform.Find("Text").GetComponent<Text>().text;
         webScript.RegisterUser(userName, token); 
-        Debug.Log("user: " + userName + ", token: " + token);
         }
 
 
--- Assets/Scripts/WebServiceScript.cs
+++ Assets/Scripts/WebServiceScript.cs
@@ -5,103 +5,114 @@ using UnityEngine.Networking;
 using System.Text;
 
 public class WebServiceScript : MonoBehaviour {
-  private string baseUrl = "http://atkytpgame.herokuapp.com/api/highscores";
-  
-  [HideInInspector]
-  public string highscores;
+    private string baseUrl = "http://atkytpgame.herokuapp.com/api/highscores";
 
-  void Start() {
-    GetHighscores();    
-  }
+    [HideInInspector]
+    public string highscores;
 
-
-  //Use this method to fetch highscores
-  public void GetHighscores() {
-    StartCoroutine(GetHighscoresText());
-  }
-
-  //Use this method to add new users
-  public void RegisterUser(string user, string token) {
-    StartCoroutine(SendUser(user, token));
-  }
-
-  //Use this method to send a highscore to the server
-  public void SendHighscore(string user, string token, int score) {
-    StartCoroutine(SendScore(user, token, score));
-  }
-
-  private IEnumerator GetHighscoresText() {
-    UnityWebRequest req = UnityWebRequest.Get(baseUrl);
-    yield return req.SendWebRequest();
-
-    if(req.isNetworkError || req.isHttpError) {
-      Debug.Log(req.error);
+    void Start() {
+        GetHighscores();
     }
-    else {
-      // Show results as text
-      Debug.Log(req.downloadHandler.text);
-      highscores = req.downloadHandler.text;
 
-      // Or retrieve results as binary data
-      byte[] results = req.downloadHandler.data;
+
+    //Use this method to fetch highscores
+    public void GetHighscores() {
+        StartCoroutine(GetHighscoresText());
     }
-  }
 
-  private IEnumerator SendUser(string user, string token) {
-    UnityWebRequest req = new UnityWebRequest(baseUrl, "POST");
-    string jsonUser = JsonifyUser(user, token, 0);
-    byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonUser);
+    //Use this method to add new users
+    public void RegisterUser(string user, string token) {
+        StartCoroutine(SendUser(user, token));
+    }
 
-    req.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
-    req.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
-    req.SetRequestHeader("Content-Type", "application/json");
+    //Use this method to send a highscore to the server
+    public void SendHighscore(int score) {
+        StartCoroutine(SendScore(score));
+    }
 
-    yield return req.Send();
+    private IEnumerator GetHighscoresText() {
+        UnityWebRequest req = UnityWebRequest.Get(baseUrl);
+        yield return req.SendWebRequest();
 
-    jsonUser = req.downloadHandler.text;
-    HighScore h = JsonUtility.FromJson<HighScore>(jsonUser);
-    Debug.Log("Username: " + h.user);
-    
-    PlayerPrefs.SetString("_id", h._id);
-    PlayerPrefs.SetString("username", h.user);
-    PlayerPrefs.SetString("token", h.token);
-    PlayerPrefs.SetInt("highScore", h.score);
-    
-    Debug.Log("------\nSaatu data:" + jsonUser);
-    Debug.Log("Status code: " + req.responseCode);
-  }
+        if (req.isNetworkError || req.isHttpError) {
+            Debug.Log(req.error);
+        } else {
+            // Show results as text
+            Debug.Log(req.downloadHandler.text);
+            highscores = req.downloadHandler.text;
 
-  private IEnumerator SendScore(string user, string token, int score) {
-    UnityWebRequest req = new UnityWebRequest(baseUrl, "PUT");
-    string jsonUser = JsonifyUser(user, token,  score);
-    byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonUser);
+            // Or retrieve results as binary data
+            byte[] results = req.downloadHandler.data;
+        }
+    }
 
-    req.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
-    req.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
-    req.SetRequestHeader("Content-Type", "application/json");
-    yield return req.Send();
-    
-    // Store updated score also locally
-    // (PlayerPrefs)
+    private IEnumerator SendUser(string user, string token) {
+        UnityWebRequest req = new UnityWebRequest(baseUrl, "POST");
+        string jsonUser = JsonifyUser(user, token, 0);
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonUser);
+        req.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        req.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        req.SetRequestHeader("Content-Type", "application/json");
 
+        yield return req.Send();
 
+        jsonUser = req.downloadHandler.text;
+        HighScore h = JsonUtility.FromJson<HighScore>(jsonUser);
+        Debug.Log("Username: " + h.user);
 
-    Debug.Log("Status code: " + req.responseCode);
-  }
+        PlayerPrefs.SetString("_id", h._id);
+        PlayerPrefs.SetString("username", h.user);
+        PlayerPrefs.SetString("token", h.token);
+        PlayerPrefs.SetInt("highScore", h.score);
 
-  private string JsonifyUser(string user, string token,  int score) {
-    HighScore h = new HighScore();
-    h.user = user;
-    h.token = token;
-    h.score = score;
-    return JsonUtility.ToJson(h);
-  }
+        Debug.Log("------\nSaatu data:" + jsonUser);
+        Debug.Log("Status code: " + req.responseCode);
+    }
 
-  [System.Serializable]
-  private class HighScore{
-    public string _id; 
-    public string user;
-    public string token;
-    public int score;
-  }
+    private IEnumerator SendScore(int score) {
+         // should we account for the possibility of not existing?
+        string id = PlayerPrefs.GetString("_id");
+
+        UnityWebRequest req = new UnityWebRequest($"{baseUrl}/{id}", "PUT");
+        string jsonHighScore = "{ \"score\": " + score + "}";
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonHighScore);
+
+        req.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        req.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        req.SetRequestHeader("Content-Type", "application/json");
+        yield return req.Send();
+
+        HighScore h = JsonUtility.FromJson<HighScore>(req.downloadHandler.text);
+        PlayerPrefs.SetInt("highScore", h.score);
+
+        Debug.Log("Status code: " + req.responseCode);
+    }
+    // This really has no relation to WebService...
+    // Relocate to own file / class? 
+    private void storeHighscoreDataLocally(HighScore h) {
+        PlayerPrefs.SetString("_id", h._id);
+        PlayerPrefs.SetString("username", h.user);
+        PlayerPrefs.SetString("token", h.token);
+        PlayerPrefs.SetInt("highScore", h.score);
+    }
+
+    private string JsonifyUser(string user, string token) {
+        return JsonifyUser(user, token, 0);
+    }
+
+    private string JsonifyUser(string user, string token, int score) {
+        HighScore h = new HighScore();
+        h.user = user;
+        h.token = token;
+        h.score = score;
+        return JsonUtility.ToJson(h);
+    }
+
+    [System.Serializable]
+    private class HighScore {
+        public string _id;
+        public string user;
+        public string token;
+        public int score;
+    }
 }
