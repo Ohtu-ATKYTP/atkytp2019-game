e2ee362f8e0cc4858ffe2d8579c77b4068306fe3
Jaakko Koli <jaakko.koli@gmail.com>
2019-02-07 23:02:31 +0200

    Follow mouse script and effects

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 p = new Vector3(Input.mousePosition[0], Input.mousePosition[1], Camera.main.transform.position.z*-1);
+        Vector3 pos = Camera.main.ScreenToWorldPoint(p);
+        pos.z = 0;
+        Debug.Log(p+" -- "+pos);
+        transform.position = pos;
+    }
+}
