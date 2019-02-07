ceb9992d46be77bd797d4dc3d412391b591ee7bc
JaakkoKoli <jaakko.koli@gmail.com>
2019-02-07 13:38:38 +0200

    Base logic works

---
+++ Assets/Scripts/TurkuGame/HitboxManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitboxManager : MonoBehaviour { 
+
+    private Hitbox[] hitboxes;
+    
+    void Start()
+    {
+        hitboxes = new Hitbox[transform.GetChildCount()];
+        int i = 0;
+        foreach (Transform child in transform)
+        {
+            hitboxes[i] = child.GetComponent<Hitbox>();
+            i++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(this.active() + " / " + this.maxAmount());
+    }
+
+    private int active()
+    {
+        int i = 0;
+        foreach(Hitbox h in this.hitboxes)
+        {
+            if (h.getActive()) i++;
+        }
+        return i;
+    }
+
+    private int maxAmount()
+    {
+        return this.hitboxes.Length;
+    }
+}
