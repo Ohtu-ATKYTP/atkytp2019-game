e482f9db1c0949788325ee0f5fae06d97f8953d9
anonOstrich <jesper.kuutti@helsinki.fi>
2019-02-08 12:48:06 +0200

    Game works when encountered in the sequence of minigames

--- Assets/Scripts/PlaceCity/PlaceCityManager.cs
+++ Assets/Scripts/PlaceCity/PlaceCityManager.cs
@@ -1,21 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlaceCityManager : MonoBehaviour {
     public Transform[] locations;
     public SpriteRenderer map;
     public float radius = 1f;
+    public int delayAfterMinigameEndsInSeconds = 2; 
     public bool drawGizmos = true;
     public Color gizmoColor = Color.cyan;
-    private GameManager gameManager; 
-  
+    public Text organisationText;
+    private DataController dataController; 
+    private bool allowTouching; 
+    private GameObject targetCity; 
+    private Dictionary<string, string> organisationsByCities;  
+    
 
     void Start() {
-        gameManager = FindObjectOfType<GameManager>();
+        allowTouching = true; 
+        dataController = FindObjectOfType<DataController>();
         for(int i = 0; i < locations.Length; i++){
             locations[i].GetComponent<CircleCollider2D>().radius = 2 * radius;        
         }
+        
+        organisationsByCities = new Dictionary<string, string>(){ 
+                {"Helsinki", "TKO-aly"},
+                {"Turku", "Asteriski" },
+                {"Tampere", "Luuppi" },
+                {"Jyväskylä", "Linkki" },
+                {"Kuopio", "Serveri" },
+                {"Oulu", "Blanko"}
+            };
+
+        targetCity = locations[((int) Random.Range(0f, 6f))].gameObject;
+        this.SetOnlyCityActive(targetCity); 
+        organisationText.text = organisationsByCities[targetCity.name];
+        activateOnlyCurrentSceneCamera(); 
+        Debug.Log("Main kameran nimi: " + Camera.main.name); 
+
+    }
+
+
+    private void activateOnlyCurrentSceneCamera(){
+            Camera[] cameras = FindObjectsOfType<Camera>();
+            for(int i = 0; i < cameras.Length; i++){ 
+                if(cameras[i].name != "PlaceCityCamera"){
+                    cameras[i].enabled = false; 
+                } else {
+                    cameras[i].enabled = true; 
+                }
+            }
+        }
+
+
+
+
+    private void SetOnlyCityActive(GameObject city){ 
+        for(int i = 0; i <locations.Length; i++){ 
+            if(locations[i].gameObject != city){
+                locations[i].gameObject.SetActive(false);    
+            }    
+        }        
     }
 
     private void Initialize() {
@@ -35,20 +81,46 @@ public class PlaceCityManager : MonoBehaviour {
     }
 
     void Update() {
+        if(!allowTouching){ 
+            return; 
+        }
         if(Input.touchCount > 0){
             Touch touch = Input.GetTouch(0);
             // z = distance from camera
             Vector3 pos = new Vector3(touch.position.x, touch.position.y, 0);
+
             pos = Camera.main.ScreenToWorldPoint(pos);
             RaycastHit2D hit; 
             hit = Physics2D.Raycast(pos, Vector2.zero);
-            
-            if(hit){
-                Debug.Log("HIT! " + hit.collider.gameObject.name);
+            GameObject city = hit ? hit.collider.gameObject : null; 
+
+            if(city != null){
+                Debug.Log("HIT! " + city.name);
+                if(city == targetCity){
+                    StartCoroutine(EndMinigame(true));
+                } else {
+                    StartCoroutine(EndMinigame(false));   
+                }
+               
             } else { 
                Debug.Log("HÄVISIT PELIN");
-                Destroy(this.gameObject);
-            }            
+                StartCoroutine(EndMinigame(false));              
+            }
+            
+            targetCity.GetComponent<InformationDisplayer>().DisplayOnMap();
+            allowTouching = false; 
         }
     }
+
+
+    private IEnumerator EndMinigame(bool win){ 
+
+        // jokin ilmoitus loppumisesta
+
+        yield return new WaitForSeconds(delayAfterMinigameEndsInSeconds); 
+        dataController.MinigameEnd(win, win ? 10 : 0);
+        }
 }
+
+
+
