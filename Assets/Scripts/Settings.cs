686ecb7cc67d51d17283c06d63b0635473a93e10
Toni Turpeinen <toni.turpeinen@helsinki.fi>
2019-02-08 13:22:31 +0200

    Added Settings screen to Main menu

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour {
+    private Text playerInfo;
+
+    void Start() {
+        playerInfo = GameObject.Find("PlayerInfo").GetComponent<Text>();
+    }
+    
+    public void updatePlayerInfo() {
+        string info = "Username: "+ PlayerPrefs.GetString("username")+"\n";
+        info += "Token: " + PlayerPrefs.GetString("token") + "\n";
+        info += "HighScore: " + PlayerPrefs.GetInt("highScore") + "\n";
+
+        playerInfo.text = info;
+    }
+}
