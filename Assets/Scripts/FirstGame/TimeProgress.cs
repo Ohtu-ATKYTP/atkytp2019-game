cb21be54d755bc5c4415fe1624fa9cdf302fbaf7
Daniel Koch <daniel.koch@helsinki.fi>
2019-02-14 16:13:48 +0200

    Visual progress bar and script

---
+++ Assets/Scripts/FirstGame/TimeProgress.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Timers;    
+using UnityEngine;
+
+public class TimeProgress : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int seconds;
+    private float timer;
+    public Slider timerSlider;
+    private DataController dataController;
+    private bool timerEnded = false;
+    void Start()
+    {
+        this.timer = (float) seconds;
+        timerSlider.value = 1f;
+        dataController = FindObjectOfType<DataController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.timer -= Time.deltaTime;
+        timerSlider.value = this.timer /seconds;
+        if (this.timer <= 0.0f && this.timerEnded == false) {
+            this.timerEnded = true;
+            timerEnd();
+        }
+
+
+    }
+
+    private void timerEnd() {
+        dataController.MinigameEnd(false, 0);
+    }
+
+
+}
