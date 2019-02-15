40fe2b781dcfe60d7d4fe3c8387de5ee6855ea1b
JaakkoKoli <jaakko.koli@gmail.com>
2019-02-15 12:06:34 +0200

    small fixes

---
+++ Assets/Scripts/TurkuGame/TouchToggleTrail.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchToggle : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            GetComponent<TrailRenderer>().emitting = true;
+        }    
+        else
+        {
+            GetComponent<TrailRenderer>().emitting = false;
+        }
+    }
+}
