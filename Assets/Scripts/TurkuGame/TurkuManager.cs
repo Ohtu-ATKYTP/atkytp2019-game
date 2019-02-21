8d568aee5b659ee01e83cbf1330fe894d8bcaabc
anonOstrich <jesper.kuutti@helsinki.fi>
2019-02-21 10:17:42 +0200

    Second game will end if time runs out

--- Assets/Scripts/TurkuGame/TurkuManager.cs
+++ Assets/Scripts/TurkuGame/TurkuManager.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TurkuManager : MonoBehaviour
-{
+public class TurkuManager : MonoBehaviour {
     public HitboxManager hitboxes;
     public FloodAnimation flood;
     private DataController dataController;
@@ -12,44 +11,37 @@ public class TurkuManager : MonoBehaviour
     private bool gameOver = false;
     public TurkuAnimation turkuAnimation;
 
-    void Start()
-    {
+    void Start() {
         dataController = FindObjectOfType<DataController>();
         StartCoroutine(Lose());
     }
 
-    void Update()
-    {
-        if (hitboxes.Active() > Mathf.Min(40, difficulty) && !gameOver)
-        {
+    void Update() {
+        if (hitboxes.Active() > Mathf.Min(40, difficulty) && !gameOver) {
             this.gameOver = true;
             this.Win();
         }
     }
 
-    public void EndMinigame(bool win)
-    {
+    public void EndMinigame(bool win) {
         dataController.SetRoundEndStatus(true);
         dataController.SetWinStatus(win);
         dataController.AddCurrentScore(10);
     }
 
-    IEnumerator Lose()
-    {
+    IEnumerator Lose() {
         yield return new WaitForSecondsRealtime(10);
         flood.StartAnimation();
         yield return new WaitForSecondsRealtime(4);
         EndMinigame(false);
     }
 
-    private void Win()
-    {
+    private void Win() {
         StartCoroutine(EndGame());
         Debug.Log("VOITIT PELIN!");
     }
 
-    IEnumerator EndGame()
-    {
+    IEnumerator EndGame() {
         turkuAnimation.StartAnimation();
         yield return new WaitForSecondsRealtime(3);
         EndMinigame(true);
