541fe70db58cc1ae938d7322c38c508325f6ff57
jesushilden <peter.e.hilden@gmail.com>
2019-02-22 13:00:16 +0200

    haalari and logo assets added and styling done

--- Assets/Scripts/LogoHaalariin/HaalariUpdater.cs
+++ Assets/Scripts/LogoHaalariin/HaalariUpdater.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 public class HaalariUpdater : MonoBehaviour {
     public Sprite tkoalyHaalari;
     public Sprite asteriskiHaalari;
+    public Sprite luuppiHaalari;
+    public Sprite serveriHaalari;
+    public Sprite linkkiHaalari;
+    public Sprite blankoHaalari;
     public void changeImage (int haalari) {
         switch (haalari) {
             case 0:
@@ -13,6 +17,18 @@ public class HaalariUpdater : MonoBehaviour {
                 break;
             case 1:
                 GetComponent<Image> ().sprite = asteriskiHaalari;
+                break;
+            case 2:
+                GetComponent<Image> ().sprite = luuppiHaalari;
+                break;
+            case 3:
+                GetComponent<Image> ().sprite = serveriHaalari;
+                break;
+            case 4:
+                GetComponent<Image> ().sprite = linkkiHaalari;
+                break;
+            case 5:
+                GetComponent<Image> ().sprite = blankoHaalari;
                 break;
             default:
                 GetComponent<Image> ().sprite = tkoalyHaalari;
--- Assets/Scripts/LogoHaalariin/LogoUpdater.cs
+++ Assets/Scripts/LogoHaalariin/LogoUpdater.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 public class LogoUpdater : MonoBehaviour {
     public Sprite tkoalyLogo;
     public Sprite asteriskiLogo;
+    public Sprite luuppiLogo;
+    public Sprite serveriLogo;
+    public Sprite linkkiLogo;
+    public Sprite blankoLogo;
     public void changeImage(int logo)
     {
         switch (logo) {
@@ -14,6 +18,18 @@ public class LogoUpdater : MonoBehaviour {
                 break;
             case 1:
                 GetComponent<Image> ().sprite = asteriskiLogo;
+                break;
+            case 2:
+                GetComponent<Image> ().sprite = luuppiLogo;
+                break;
+            case 3:
+                GetComponent<Image> ().sprite = serveriLogo;
+                break;
+            case 4:
+                GetComponent<Image> ().sprite = linkkiLogo;
+                break;
+            case 5:
+                GetComponent<Image> ().sprite = blankoLogo;
                 break;
             default:
                 GetComponent<Image> ().sprite = tkoalyLogo;
