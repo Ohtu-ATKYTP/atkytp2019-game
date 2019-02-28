04e717ffad1f9dcb42ba9b5641c63ef6642d7f9a
Toni Turpeinen <toni.turpeinen@helsinki.fi>
2019-02-28 13:19:10 +0200

    Formcontroller modded
    Scenemager, Menuscene done

--- Assets/Scripts/RegisterButton.cs
+++ Assets/Scripts/RegisterButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class FormController : MonoBehaviour {
+public class RegisterButton : MonoBehaviour {
     public Text statusMessage;
     private WebServiceScript webScript;
 
