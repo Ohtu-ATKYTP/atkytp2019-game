916632ace8a887efaea2616bf2b0ea51cd2ca6b3
anonOstrich <jesper.kuutti@helsinki.fi>
2019-02-28 13:07:13 +0200

    The very basics of general minigame logic
    Also connected the timebar to the minigame logic, to call a method when  the time runs out.

--- Assets/Scripts/Utilities/MinigameLogic.cs
+++ Assets/Scripts/Utilities/MinigameLogic.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MinigameLogic : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class MinigameLogic : MonoBehaviour {
+    private DataController dataController;
+    [SerializeField]
+    [Tooltip("Higher values mean more difficult minigame")]
+    private int difficulty;
+    [SerializeField]
+    private int pointsForWinning = 10;
+
+    void Start() {
+        dataController = FindObjectOfType<DataController>();
+        // dataController.GetDifficulty();
+        difficulty = -1;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    public void OnTimerEnd() {
+        // If something additional to losing the game needs to happen when the time runs out, 
+        // those actions should be specified before the call to EndMinigame
+
+        // Placeholder: spawn a sphere at the origin of the world space
+        //GameObject sphereOfWastedTime = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+
+
+        // Time running out will result in a lost game
+        EndMinigame(false);
+    }
+
+
+    public void EndMinigame(bool won) {
+        dataController.MinigameEnd(won,
+                won ? pointsForWinning : 0
+            );
+
     }
 }
