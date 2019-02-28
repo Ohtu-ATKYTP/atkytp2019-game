fd1b373e4b04e97388f667747d90ee5988dfd496
anonOstrich <jesper.kuutti@helsinki.fi>
2019-02-28 13:55:11 +0200

    Basis for handling waiting after the end

--- Assets/Scripts/Utilities/MinigameLogic.cs
+++ Assets/Scripts/Utilities/MinigameLogic.cs
@@ -1,19 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Threading.Tasks;
 using UnityEngine;
 
 public class MinigameLogic : MonoBehaviour {
     private DataController dataController;
     [SerializeField]
-    [Tooltip("Higher values mean more difficult minigame")]
-    private int difficulty;
+    [Tooltip("Higher values mean more difficult minigame. Will be requested from the data manager in the game, but set here if you want to test only this scene.")]
+    private int difficulty = 1;
     [SerializeField]
     private int pointsForWinning = 10;
+    [Tooltip("The delay between the time of losing/winning, and the time of unloading the minigame scene (in seconds)")]
+    public int delayAfterMinigameEndsInSeconds = 2;
+
+
 
     void Start() {
         dataController = FindObjectOfType<DataController>();
-        // dataController.GetDifficulty();
-        difficulty = -1;
+        difficulty = dataController.GetDifficulty();
     }
 
 
@@ -22,19 +24,30 @@ public class MinigameLogic : MonoBehaviour {
         // those actions should be specified before the call to EndMinigame
 
         // Placeholder: spawn a sphere at the origin of the world space
-        //GameObject sphereOfWastedTime = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        //GameObject go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 
-
-
-        // Time running out will result in a lost game
         EndMinigame(false);
     }
 
 
-    public void EndMinigame(bool won) {
+
+    // Implement the methods for keeping track of the status of the goals and player progress
+    // Or in another script that calls the methods of this one
+
+
+
+    public async void EndMinigame(bool won) {
+        // Ensuring that the timer is always stopped when the minigame ends
+        FindObjectOfType<TimeProgress>().StopTimerProgression();
+
+        // Here: actions that are made in the minigame once the result is certain, e.g. text of success, starting ending animation 
+        Debug.Log("The scene adapts to ending");
+
+        await Task.Delay(1000 * delayAfterMinigameEndsInSeconds);
+
+        Debug.Log("The scene is ready to be changed.");
         dataController.MinigameEnd(won,
                 won ? pointsForWinning : 0
             );
-
     }
 }
