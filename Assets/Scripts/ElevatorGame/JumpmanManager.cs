3c15b606c0bdc58ba509714ca906a1ea7f989e7f
Toni Turpeinen <toni.turpeinen@helsinki.fi>
2019-03-01 16:37:33 +0200

    Borders, jumpmen, button
    Trying to get script working

---
+++ Assets/Scripts/ElevatorGame/JumpmanManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpmanManager : MonoBehaviour{   
+    
+    private GameObject[] JumpmanList;
+    private Rigidbody selfRigidbody;
+    void Start() {
+        JumpmanList =  GameObject.FindGameObjectsWithTag("Jumpman");
+    }
+
+    void Update() {
+    }
+
+    public void Jump(){
+        while(true){
+
+            foreach (GameObject Jumper in JumpmanList){
+                Jumper.rigidbody.AddForce(0, forceConst, 0, ForceMode.Impulse);
+                //selfRigidbody.AddForce(0, forceConst, 0, ForceMode.Impulse);
+            }
+        WaitForSecondsRealtime(10);
+        }
+    }
+}
