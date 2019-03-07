a53acdbef7e4df1df3e6e4adb5d1eec63418b50a
Toni Turpeinen <toni.turpeinen@helsinki.fi>
2019-03-07 14:38:57 +0200

    Collision to bottom border

---
+++ Assets/Scripts/ElevatorGame/CollisionLogic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionLogic : MonoBehaviour
+{
+    private ElevatorGameLogic EGLogic;
+    
+    void Start() {
+        this.EGLogic = FindObjectOfType<ElevatorGameLogic>();
+    }
+    void OnCollisionEnter2D(Collision2D collision2D){
+        EGLogic.AddDamage();
+    }
+}
--- Assets/Scripts/ElevatorGame/JumpmanLogic.cs
+++ Assets/Scripts/ElevatorGame/JumpmanLogic.cs
@@ -5,20 +5,11 @@ using Image = UnityEngine.UI.Image;
 
 public class JumpmanLogic : MonoBehaviour {
     
-    private ElevatorGameLogic EGLogic;
     public Sprite Face;
-    void Start() {
-        this.EGLogic = FindObjectOfType<ElevatorGameLogic>();
-    }
-    void Update() {
-    }
-    void OnCollisionEnter2D(Collision2D collision2D){
-        EGLogic.AddDamage();
-    }
-
     public void Jump() {
         GetComponent<Rigidbody2D>().AddForce(Vector2.up*3000, ForceMode2D.Impulse);
     }
+    
     public void ChangeFace(){
         GetComponent<Image>().sprite = Face;
         GetComponent<Image>().color = Color.white;
