83f9ab1ec97174b9b0ae4e33205e35000295c43a
anonOstrich <jesper.kuutti@helsinki.fi>
2019-03-07 11:59:18 +0200

    Created the script for enabling 'cheats'

---
+++ Assets/Scripts/MinigameDevCheats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameDevCheats : MonoBehaviour {
+
+    void Start() {
+
+    }
+
+    void Update() {
+        
+        if(Input.GetKeyDown("space")){ 
+            
+        } else if(Input.GetKeyDown(KeyCode.W)){ 
+            
+        } else if(Input.GetKeyDown(KeyCode.L)){
+             
+       }
+    }
+
+    void 
+}
