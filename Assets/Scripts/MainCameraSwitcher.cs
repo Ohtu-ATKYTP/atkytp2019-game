5e03a9ebf85c3e4ef421cafc37ba32bfbc307fcc
anonOstrich <jesper.kuutti@helsinki.fi>
2019-03-16 22:44:56 +0200

    Refactoring to help tests and first tests

--- Assets/Scripts/MainCameraSwitcher.cs
+++ Assets/Scripts/MainCameraSwitcher.cs
@@ -1,34 +1,83 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 /*
  * The main camera is used to calculate the positions of the touches. Therefore it is important to ensure that the camera of this scene is the main camera. 
  * Without the script the camera in the very first scene will be the main camera
  * 
- * 
- * 
  */
-public class MainCameraSwitcher : MonoBehaviour {
-    private Camera initialMainCamera; 
+public class MainCameraSwitcher : MonoBehaviour, ICameraController {
+    private MainCameraSwitcherLogic logic;
 
-    void OnEnable() {
-        initialMainCamera = Camera.main;
-        activateOnlyCamera(GetComponent<Camera>());
+    // Callback methods that the game engine will call at appropriate times
+    // Must be here, since they are called  on every MonoBehaviour attached to game objects
+    private void OnEnable() {
+        logic = new MainCameraSwitcherLogic();
+        // this class is an implementation of ICameraController, so it can be set in the following way
+        logic.SetCameraController(this);
+        logic.Initialize();
     }
-
 
     private void OnDestroy() {
-        activateOnlyCamera(initialMainCamera);
+        logic.ResetMainCamera();
     }
 
-        private void activateOnlyCamera(Camera camera) {
-        Camera[] cameras = FindObjectsOfType<Camera>();
+
+
+    // Implements the interface: offers connections to the game engine
+    public Camera[] FetchCameras() {
+        return FindObjectsOfType<Camera>();
+    }
+
+    public Camera FetchMainCamera() {
+        return Camera.main;
+    }
+
+    public Camera FetchAttachedCamera() {
+        return GetComponent<Camera>();
+    }
+}
+
+
+// methods that must use the game engine
+public interface ICameraController {
+    Camera FetchMainCamera();
+    Camera[] FetchCameras();
+    Camera FetchAttachedCamera();
+}
+
+// Logic that is not directly coupled with the game engine
+// This is presumably what we want to actually test!
+[Serializable]
+public class MainCameraSwitcherLogic {
+    private ICameraController cameraController;
+    private Camera initialMainCamera;
+
+
+    public void SetCameraController(ICameraController ctrl) {
+        this.cameraController = ctrl;
+
+    }
+
+    public void Initialize() {
+        initialMainCamera = cameraController.FetchMainCamera();
+        ActivateOnlyCamera(cameraController.FetchAttachedCamera());
+    }
+
+
+    public void ResetMainCamera() {
+        ActivateOnlyCamera(initialMainCamera);
+    }
+
+    public void ActivateOnlyCamera(Camera camera) {
+        Camera[] cameras = cameraController.FetchCameras();
         for (int i = 0; i < cameras.Length; i++) {
-            if (cameras[i] != camera) {
-                cameras[i].enabled = false;
+            if (cameras[i] == camera) {
+                if (cameras[i] != null) {
+                    cameras[i].enabled = true;
+                }
             } else {
-                cameras[i].enabled = true;
+                cameras[i].enabled = false;
             }
         }
     }
---
+++ Assets/Tests/EditModeTests/PlaceCity/CameraSwitcherTest.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NSubstitute;
+
+namespace Tests {
+    public class CameraSwitcherTest {
+
+        [Test]
+        public void InitializationCallsCorrectMethods() {
+            ICameraController stubController = Substitute.For<ICameraController>();
+
+            MainCameraSwitcherLogic logic = new MainCameraSwitcherLogic();
+            logic.SetCameraController(stubController);
+
+            stubController.Received(0).FetchMainCamera();
+            stubController.Received(0).FetchCameras();
+            stubController.Received(0).FetchAttachedCamera();
+            logic.Initialize();
+            stubController.Received(1).FetchMainCamera();
+            stubController.Received(1).FetchCameras();
+            stubController.Received(1).FetchAttachedCamera();
+        }
+
+        [Test]
+        public void ActivateOnlyCameraActivatesAttachedCameraCorrectly() {
+            Camera cam = new Camera();
+           
+            ICameraController stubController = Substitute.For<ICameraController>();
+            stubController.FetchCameras().Returns(new[] { cam });
+            stubController.FetchAttachedCamera().Returns(cam);
+
+            MainCameraSwitcherLogic logic = new MainCameraSwitcherLogic();
+            logic.SetCameraController(stubController);
+            logic.ActivateOnlyCamera(cam);
+
+            stubController.Received(1).FetchCameras();
+           // Assert.IsTrue(cam.enabled);
+        }
+
+        [Test]
+        public void ActivateOnlyCameraDoesNotDisableMainCameraIfItIsAttached() { }
+
+        [Test]
+        public void ActivateonlyCameraWillDisableCamerasThatAreNotParameter() { }
+
+        [Test]
+        public void ResetMainCameraWorksWhenCameraWasNotDisabled() { }
+
+        [Test]
+        public void ResetMainCameraWillEnableTheInitialMainCamera() { }
+
+        [Test]
+        public void CameraSwitcherTestSimplePasses() {
+            // Use the Assert class to test conditions
+            Assert.That(true);
+        }
+    }
+}
