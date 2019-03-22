f3582933b70a05a0d7d02f27244406fb73c0502b
jesushilden <peter.e.hilden@gmail.com>
2019-03-22 15:46:50 +0200

    MenuManager refactoring

--- Assets/Scripts/MainMenu/MenuManager.cs
+++ Assets/Scripts/MainMenu/MenuManager.cs
@@ -3,62 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour {
-    [SerializeField] private Canvas registrationCanvas; 
-    [SerializeField] private Canvas mainMenuCanvas; 
-    [SerializeField] private Canvas highScoreCanvas; 
-    [SerializeField] private Canvas settingsCanvas;
 
-    Canvas[] canvases;
+    private Dictionary<string, Canvas> screens = new Dictionary<string, Canvas>{};
+
+    private string current = "Main Menu Screen";
 
     private void Start() {
-        canvases = new Canvas[4];
-        canvases[0] = registrationCanvas; 
-        canvases[1] = mainMenuCanvas; 
-        canvases[2] = highScoreCanvas; 
-        canvases[3] = settingsCanvas; 
-        setUpMenus();
-        //HSmanager.sync()
-    }
-    private void setUpMenus() {
-        foreach(Canvas c in canvases) {
+        screens["Registration Screen"] = GameObject.Find("Registration Screen").GetComponent<Canvas>();
+        screens["Main Menu Screen"] = GameObject.Find("Main Menu Screen").GetComponent<Canvas>();
+        screens["High Score Screen"] = GameObject.Find("High Score Screen").GetComponent<Canvas>();
+        screens["Settings Screen"] = GameObject.Find("Settings Screen").GetComponent<Canvas>();
+
+        foreach(Canvas c in screens.Values) {
             c.enabled = false;
         }
-        if(PlayerPrefs.HasKey("registered")){ 
-            mainMenuCanvas.enabled = true;      
-        } else {
-            registrationCanvas.enabled = true;
+        
+        screens["Main Menu Screen"].enabled = true;
+        current = "Main Menu Screen";
+
+        if(!PlayerPrefs.HasKey("registered")){
+            displayOnlyMenu("Registration Screen");
             PlayerPrefs.SetInt("registered", 0);
         }
     }
 
-
-
     public void displayOnlyMenu(string canvasName) {
-        string nameOfCanvas = "ERROR"; 
-        if(canvasName.Equals(registrationCanvas.name)){
-            nameOfCanvas = "REGISTER";     
-        } else if(canvasName.Equals(mainMenuCanvas.name)){
-            nameOfCanvas = "MAIN MENU"; 
-        } else if(canvasName.Equals(highScoreCanvas.name)){
-            nameOfCanvas = "HIGH SCORE"; 
-        } else if(canvasName.Equals(settingsCanvas.name)){
-            nameOfCanvas = "SETTINGS"; 
-        }
-
-        Debug.Log("Will show menu " + nameOfCanvas);
-        setOneCanvasActive(canvasName); 
-            
+        screens[current].enabled = false;
+        screens[canvasName].enabled = true;
+        current = canvasName;
     }
-
-    private void setOneCanvasActive(string name){ 
-            foreach(Canvas c in canvases){ 
-                if(c.name.Equals(name)){
-                    c.enabled = true; 
-                    Debug.Log("Is set active");
-                } else {
-                    c.enabled = false; 
-                }
-            }
-        }
 
 }
