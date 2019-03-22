6b432188982cc76808d05463520f3ce8d24212a8
anonOstrich <jesper.kuutti@helsinki.fi>
2019-03-22 11:10:19 +0200

    Saving work

--- Assets/Scripts/Tools/ComponentCreator.cs
+++ Assets/Scripts/Tools/ComponentCreator.cs
@@ -1,14 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public static class ComponentCreator{
-    
-    public static T Create<T>() where T : Component{ 
-        
-        GameObject go = new GameObject();        
-        go.AddComponent<T>() ;
+public static class ComponentCreator {
+
+    public static T Create<T>() where T : Component {
+        GameObject go = new GameObject();
+        go.AddComponent<T>();
         return go.GetComponent<T>();
+    }
 
-       }
+    public static T Create<T>(GameObject go) where T : Component {
+        go.AddComponent<T>();
+        return go.GetComponent<T>();
+    }
 }
