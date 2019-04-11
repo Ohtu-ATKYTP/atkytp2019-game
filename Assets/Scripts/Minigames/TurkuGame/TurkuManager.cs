8cf049396edfca593e04e1d13e3022569ba24cd8
JaakkoKoli <jaakko.koli@gmail.com>
2019-04-11 14:00:27 +0300

    Updated visuals and difficulty

--- Assets/Scripts/Minigames/TurkuGame/TurkuManager.cs
+++ Assets/Scripts/Minigames/TurkuGame/TurkuManager.cs
@@ -18,7 +18,7 @@ public class TurkuManager : MonoBehaviour, IMinigameEnder {
     }
 
     void Update() {
-        if (hitboxes.Active() > Mathf.Min(hitboxes.HitboxCount()*0.95f, (difficulty*dataController.GetDifficulty())) && !gameOver && !lineUsedUp) {
+        if (hitboxes.Active() > Mathf.Min(hitboxes.HitboxCount()*0.95f, (difficulty*(dataController.GetDifficulty()+2))) && !gameOver && !lineUsedUp) {
             WinMinigame();
         }
     }
