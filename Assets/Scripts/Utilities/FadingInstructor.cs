e2b29a4978b9debbc129f7c4fa0d299497ec6dc4
anonOstrich <jesper.kuutti@helsinki.fi>
2019-04-13 20:43:31 +0300

    Gyro instructions fade with time, settable from the editor inspector

--- Assets/Scripts/Utilities/FadingInstructor.cs
+++ Assets/Scripts/Utilities/FadingInstructor.cs
@@ -6,35 +6,37 @@ using UnityEngine.UI;
 
 public class FadingInstructor : MonoBehaviour {
 
-    public float fadeDuration = 5f;
+    public float fadeDuration = 1f;
     private Text[] texts;
     private SpriteRenderer[] sprites;
 
     void Start() {
-        texts = GetComponents<Text>();
-        sprites = GetComponents<SpriteRenderer>();
+        texts = GetComponentsInChildren<Text>();
+        sprites = GetComponentsInChildren<SpriteRenderer>();
         StartCoroutine(CORFadeAway());
     }
 
+
+
     private IEnumerator CORFadeAway() {
-        float t = 0;
-        Color[] startingTextColors = texts.Select(txt => txt.color).ToArray();
-        Color[] goalTextColors = startingTextColors.Select(c => new Color(c.r, c.g, c.b, 100)).ToArray();
-        Color[] startingSpriteColors = sprites.Select(sprt => sprt.color).ToArray();
-        Color[] goalSpriteColors = startingSpriteColors.Select(c => new Color(c.r, c.g, c.b, 100)).ToArray();
+        float t = 1f;
+
 
 
         do {
             yield return null;
-            t += Time.deltaTime / fadeDuration;
+            t -= Time.deltaTime / fadeDuration;
             for (int i = 0; i < texts.Length; i++) {
-                texts[i].color = Color.Lerp(startingTextColors[i], goalTextColors[i], t);
+                texts[i].color = new Color(texts[i].color.r, texts[i].color.g, texts[i].color.b, t);
+                texts[i].SetAllDirty();
             }
             for (int i = 0; i < sprites.Length; i++) {
-                sprites[i].color = Color.Lerp(startingSpriteColors[i], goalSpriteColors[i], t);
+                sprites[i].color = new Color(sprites[i].color.r, sprites[i].color.g, sprites[i].color.b, t);
             }
-        } while (t <= 1);
-
+            Debug.Log("T: " + t);
+            Debug.Log("Counts: " + texts.Length + sprites.Length);
+        } while (t >= 0);
+        Debug.Log("DONE");
 
 
     }
