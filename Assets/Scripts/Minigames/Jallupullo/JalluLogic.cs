89c62a48748577d6f3b0cd8588844942b3de4a1c
anonOstrich <jesper.kuutti@helsinki.fi>
2019-04-17 12:19:45 +0300

    Initial scene and game logic script

---
+++ Assets/Scripts/Minigames/Jallupullo/JalluLogic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JalluLogic : MinigameLogic
+{
+    protected override void DisplayEndingActions(bool won) {
+        string message = won ? "You won!" : "You lost :/";
+        Debug.Log(message);
+    }
+
+    protected override void ConfigureDifficulty(int difficulty) {
+        Debug.Log("The difficulty of the Jallu shall be: " + difficulty);
+    }
+}
