9568411768c8218a7b99e53e95a8b13b9ff94132
anonOstrich <jesper.kuutti@helsinki.fi>
2019-04-25 13:08:01 +0300

    Created higher resolution logos

--- Assets/Scripts/Minigames/PlaceCity/OrganizationDisplayer.cs
+++ Assets/Scripts/Minigames/PlaceCity/OrganizationDisplayer.cs
@@ -3,17 +3,14 @@ using UnityEngine.UI;
 
 public class OrganizationDisplayer : MonoBehaviour {
     public OrganizationInfos orgInfo;
-    Image logo;
-    Text orgName;
+    private Image logo;
 
     void Awake() {
         logo = GetComponent<Image>();
-        orgName = GetComponentInChildren<Text>();
     }
 
     public void Initialize(string name) {
         OrganizationInfo info = orgInfo.GetOrgByName(name);
-        orgName.text = info.name;
         logo.sprite = info.logo;
     }
 }
